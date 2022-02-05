e5d84d7944bb41ebae515879f15a919706912685
josiah <jklaivins@gmail.com>
2022-02-05 00:59:22 -0500

    Added: - working exception filter, looks nice! Only works in notebooks though, but I think that actually might be for the best.

---
+++ fastrl/None.py
@@ -0,0 +1,16 @@
+
+
+# Cell
+# Python native modules
+import os,sys
+from copy import deepcopy,copy
+from typing import *
+import types
+import logging
+import inspect
+# Third party libs
+from fastcore.all import *
+import numpy as np
+# Local modules
+
+_logger=logging.getLogger(__name__)
