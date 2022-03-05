552753731986b506fad683419f5f593a25232c10
josiahls <jklaivins@gmail.com>
2022-03-05 16:45:35 -0500

    Added: - dataloader2 call. We might be able to use this instead of a custom dataloader - initial datablock. Trying to simplify it. We'll see how much we need to readd. I'm trying to remove assumptions of >1 inputs/outputs and instead allow a specific data block for that if needed. Also thinking of removign the usage of DataLoaders and Datasets and instead just use L for them if we want aggregate behavior. We'll see how this goes.

--- fastrl/None.py
+++ fastrl/None.py
@@ -79,3 +79,83 @@ class _FakeLoader:
         finally: self.num_workers = old_num_workers
 
 _collate_types = (ndarray, Tensor, typing.Mapping, str)
+
+# Cell
+from fastai.torch_basics import *
+from torch.utils.data.dataloader import _MultiProcessingDataLoaderIter,_SingleProcessDataLoaderIter,_DatasetKind
+_loaders = (_MultiProcessingDataLoaderIter,_SingleProcessDataLoaderIter)
+
+# Cell
+def _wif(worker_id):
+    set_num_threads(1)
+    info = get_worker_info()
+    ds = info.dataset.d
+    ds.num_workers,ds.offs = info.num_workers,info.id
+    set_seed(info.seed)
+    ds.wif()
+
+class _FakeLoader:
+    def _fn_noops(self, x=None, *args, **kwargs): return x
+
+    _IterableDataset_len_called,_auto_collation,collate_fn,drop_last = None,False,_fn_noops,False
+    _index_sampler,generator,prefetch_factor  = Inf.count,None,2
+    dataset_kind = _dataset_kind = _DatasetKind.Iterable
+
+    def __init__(self, d, pin_memory, num_workers, timeout, persistent_workers):
+        self.dataset,self.default,self.worker_init_fn = self,d,_wif
+        store_attr('d,pin_memory,num_workers,timeout,persistent_workers')
+
+    def __iter__(self): return iter(self.d.create_batches(self.d.sample()))
+
+    @property
+    def multiprocessing_context(self): return (None,multiprocessing)[self.num_workers>0]
+
+    @contextmanager
+    def no_multiproc(self):
+        old_num_workers = self.num_workers
+        try:
+            self.num_workers = 0
+            yield self.d
+        finally: self.num_workers = old_num_workers
+
+_collate_types = (ndarray, Tensor, typing.Mapping, str)
+
+# Cell
+from fastai.torch_basics import *
+from torch.utils.data.dataloader import _MultiProcessingDataLoaderIter,_SingleProcessDataLoaderIter,_DatasetKind
+_loaders = (_MultiProcessingDataLoaderIter,_SingleProcessDataLoaderIter)
+
+# Cell
+def _wif(worker_id):
+    set_num_threads(1)
+    info = get_worker_info()
+    ds = info.dataset.d
+    ds.num_workers,ds.offs = info.num_workers,info.id
+    set_seed(info.seed)
+    ds.wif()
+
+class _FakeLoader:
+    def _fn_noops(self, x=None, *args, **kwargs): return x
+
+    _IterableDataset_len_called,_auto_collation,collate_fn,drop_last = None,False,_fn_noops,False
+    _index_sampler,generator,prefetch_factor  = Inf.count,None,2
+    dataset_kind = _dataset_kind = _DatasetKind.Iterable
+
+    def __init__(self, d, pin_memory, num_workers, timeout, persistent_workers):
+        self.dataset,self.default,self.worker_init_fn = self,d,_wif
+        store_attr('d,pin_memory,num_workers,timeout,persistent_workers')
+
+    def __iter__(self): return iter(self.d.create_batches(self.d.sample()))
+
+    @property
+    def multiprocessing_context(self): return (None,multiprocessing)[self.num_workers>0]
+
+    @contextmanager
+    def no_multiproc(self):
+        old_num_workers = self.num_workers
+        try:
+            self.num_workers = 0
+            yield self.d
+        finally: self.num_workers = old_num_workers
+
+_collate_types = (ndarray, Tensor, typing.Mapping, str)
